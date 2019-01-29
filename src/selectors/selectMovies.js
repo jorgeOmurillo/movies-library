0e72eab9342e65ef75a1cb40dd37534dd7f9a9b3
Jorge Murillo <j.orlandomurillo@gmail.com>
2019-01-29 17:38:08 +0800

    Fix Genre list

--- src/selectors/selectMovies.js
+++ src/selectors/selectMovies.js
@@ -12,6 +12,26 @@ export default (movies, { text, filterBy }) => {
       switch (filterBy) {
         case 'action':
           return movie.genre === 'Action';
+        case 'animation':
+          return movie.genre === 'Animation';
+        case 'adventure':
+          return movie.genre === 'Adventure';
+        case 'comedy':
+          return movie.genre === 'Comedy';
+        case 'crime':
+          return movie.genre === 'Crime';
+        case 'drama':
+          return movie.genre === 'Drama';
+        case 'fantasy':
+          return movie.genre === 'Fantasy';
+        case 'horror':
+          return movie.genre === 'Horror';
+        case 'romance':
+          return movie.genre === 'Romance';
+        case 'science_fiction':
+          return movie.genre === 'Science Fiction';
+        case 'thriller':
+          return movie.genre === 'Thriller';
         default:
           return movie;
       }
